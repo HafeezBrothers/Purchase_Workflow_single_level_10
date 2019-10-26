8933bc017d358e56cb38090462437cfb3d83c5dd
HafeezBrothers <mailto:hafeezbrothers.odoo@gmail.com>
2019-10-26 16:12:38 +0500

    First commit Message

---
+++ po_workflows/__manifest__.py
@@ -0,0 +1,20 @@
+{
+
+    'name':'Purchase Order WorkFlows',
+    'description' : 'PO WorkFlows',
+    'author' : 'Hafeez Brothers',
+    
+    'depends' : [
+                   'base', 'product','purchase',
+                ],
+    'data' :[   
+                
+                'views/purchase_cus.xml',
+                'security/hr_user_rights1.xml'
+            ],
+    'installable' : True,
+    'price':25.00,
+    'currency':'EUR', 
+   
+
+}
